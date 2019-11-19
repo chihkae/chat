33acad9df1ad13471aa31930b3f4878b673bf300
Wenshi Wang <wenshiwang@dhcp-20-64-32-5c-c2-58.ubcvisitor.wifi.ubc.ca>
2019-11-18 19:41:32 -0800

    server first commit

--- ncTh.c
+++ ncTh.c
@@ -7,6 +7,29 @@
 #include <string.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define PORT "3490" // the port client will be connecting to
+#define PARSE_OK 
+
+#define MAXDATASIZE 100 // max number of bytes we can get at once
+#define MAX_CLIENTS 10
+
+void *get_in_addr(struct sockaddr *sa){
+  return &(((struct sockaddr_in*)sa)->sin_addr);
+}
+struct connection connections[10];
 
 int main(int argc, char **argv) {
   
@@ -15,6 +38,33 @@ int main(int argc, char **argv) {
   
   struct commandOptions cmdOps;
   int retVal = parseOptions(argc, argv, &cmdOps);
+  int sockfd;
+  if(retVal == PARSE_OK) {
+    for()
+       sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1) { 
+        printf("socket creation failed...\n"); 
+        exit(0); 
+    } 
+    else
+        memset() 
+  }
+
+  int sockfd, numbytes;
+  char buf[MAXDATASIZE];
+  struct addrinfo hints, *servinfo, *p;
+  int rv;
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+
+  if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+    // fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
+
+  
   printf("Command parse outcome %d\n", retVal);
 
   printf("-k = %d\n", cmdOps.option_k);
---
+++ testServer.c
@@ -0,0 +1,4 @@
+struct{
+    int socketFD;
+    bool inUse;
+};
